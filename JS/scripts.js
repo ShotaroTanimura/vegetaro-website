62f012fef5cdc65aea1c47073f5b3b91808e6a5e
Shotaro Tanimura <144506009+ShotaroTanimura@users.noreply.github.com>
2023-10-23 22:53:04 +0900

    web site update

---
+++ JS/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  let balloons = $(".balloon");
+  let isBalloonShown = false;
+
+  $(window).on('scroll', function() {
+      let scrollTop = $(this).scrollTop();
+      if (!isBalloonShown && scrollTop > 100) { 
+          // Show balloons in order with a delay between each
+          balloons.each(function(index, balloon) {
+              setTimeout(function() {
+                  $(balloon).css('opacity', '1').animate({bottom: '+=17vw'}, 1000);
+              }, index * 400); // 1000ms delay between each balloon
+          });
+
+          isBalloonShown = true;
+      }
+  });
+});
